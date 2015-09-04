b9c9765e1749625ddebd8a63784d9b2249c841eb
jrudel <jesse.rudel@gmail.com>
2015-09-03 23:30:17 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+function showIt()
+{
+ var osel = document.getElementById("sel1");
+ var ote = document.getElementById("op1");
+ var ste = document.getElementById("op2");
+ ote.style.visibility = "visible";
+ ote.innerText = "Your chosen option is: "+osel.value;
+ if(osel.selectedIndex === 0)
+   {
+     document.getElementById("swoop").innerHTML = '<iframe width="560" height="315" src="https://www.youtube.com/embed/IK3bG1OQeLw" frameborder="0" allowfullscreen></iframe>';
+   }
+ else if(osel.selectedIndex === 1)
+   {
+     document.getElementById("swoop").innerHTML = '<iframe width="560" height="315" src="https://www.youtube.com/embed/xCNSwbanIVA" frameborder="0" allowfullscreen></iframe>';
+   }
+}
