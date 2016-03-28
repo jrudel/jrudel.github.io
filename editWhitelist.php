f7c0fb90aea5c597f2c5d1f3d2657e9f3e8c3d8e
jrudel <jesse.rudel@gmail.com>
2016-03-28 13:54:11 -0300

    Editing headers

--- editWhitelist.php
+++ editWhitelist.php
@@ -1,4 +1,8 @@
 <?php
-header('Allow-Control-Allow-Origin: *');
+header('Access-Control-Allow-Origin: http://webdavserver.com');
+header('Access-Control-Allow-Credentials: true');
+header('Access-Control-Allow-Methods: ACL, CANCELUPLOAD, CHECKIN, CHECKOUT, COPY, DELETE, GET, HEAD, LOCK, MKCALENDAR, MKCOL, MOVE, OPTIONS, POST, PROPFIND, PROPPATCH, PUT, REPORT, SEARCH, UNCHECKOUT, UNLOCK, UPDATE, VERSION-CONTROL');
+header('Access-Control-Allow-Headers: Overwrite, Destination, Content-Type, Depth, User-Agent, Translate, Range, Content-Range, Timeout, X-File-Size, X-Requested-With, If-Modified-Since, X-File-Name, Cache-Control, Location, Lock-Token, If');
+header('Access-Control-Expose-Headers: DAV, content-length, Allow');
 $whitelist =  $_POST['whitelist'];
 echo $whitelist;
